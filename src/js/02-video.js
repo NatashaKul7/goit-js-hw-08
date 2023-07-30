db4d96189e863e7c0fcf79b92783e3a2c1057f62
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-07-30 20:00:43 +0200

    3task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,4 @@
 import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const iframePlayer = new Vimeo.Player(iframe);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (savedData) { 
+    form.elements.email.value = savedData.email;
+    form.elements.message.value = savedData.message;
+};
+
+function onInput () { 
+    const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+};
+
+function onSubmit(e) { 
+    e.preventDefault();
+    
+    const data = localStorage.getItem('feedback-form-state');
+  if (!data) {
+    return;
+  }
+  console.log(JSON.parse(data));
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
+
+
+
+
+
+
+
+
+
+
+
 
